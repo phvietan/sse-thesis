c7b40967b32ec7314b8636405d04b5d793618244
regod <phvietan@gmail.com>
2020-03-11 17:25:51 +0700

    try to upload using golang

---
+++ backend/tes.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func UploadFile(w http.ResponseWriter, r *http.Request) {
+	file, handler, err := r.FormFile("file")
+	fileName := r.FormValue("file_name")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	_, _ = io.WriteString(w, "File "+fileName+" Uploaded successfully")
+	_, _ = io.Copy(f, file)
+}
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "Welcome home!")
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/file", UploadFile).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", router))
+}
--- frontend/src/pages/upload.jsx
+++ frontend/src/pages/upload.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import Menubar from '../components/menubar';
+import { AESHandler } from './crypto/aeshandler.js';
 
 const storageController = require('../controller/controller_storage');
 
 // This App here is for routing purposes
+function encryptFile() {
+	const fileInput = document.getElementById('file');
+	const file = fileInput.files[0]
+	const reader = new FileReader();
+	const { aesKey } = storageController.getKeys();
+	reader.onload = function(e) {
+		document.getElementById('wait').innerHTML="Please Wait Encrypting";
+		const ciphertext = AESHandler.Encrypt(aesKey, e.target.result);
+		document.getElementById('wait').innerHTML="File Encryption Finished";
+    console.log([ciphertext]);
+    
+		const data = new FormData($("#fileinfo")[0]);
+		const encryptedFile = new File([encrypted], file.name + '.encrypted', {type: "text/plain", lastModified: new Date()});
+		data.append('file[0]', encryptedFile);
+    document.getElementById('wait').innerHTML= "File Upload Starting";
+    
+		
+	};
+	reader.readAsDataURL(file);
+}
+
 class Upload extends Component {
   constructor(props) {
     super(props);
